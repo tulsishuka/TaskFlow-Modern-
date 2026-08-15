eba024b06adf820720a20d0b25934ac25b1745ce
Tulasi <tulsishuklag@gmail.com>
2026-08-15 23:13:41 +0530

    server.ts

---
+++ Backend/server.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async (): Promise<void> => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server");
+    process.exit(1);
+  }
+};
+
+startServer();
